25297542bca3edd2b22611c29792cda6c3507a6c
Neruthes <i@neruthes.xyz>
2024-10-02 20:13:32 +0000

    Regular update '2024-10-02T20:13:32+00:00'

--- inwebconsole.js
+++ inwebconsole.js
@@ -3,13 +3,23 @@
 
 
 (function () {
-    const output_text = [].map.call(document.querySelectorAll('tr.app'), function (tr_node) {
-        const appid = tr_node.getAttribute('data-appid');
-        const date = tr_node.querySelector('td:nth-child(7)').getAttribute('data-sort');
-        const title = tr_node.querySelector('td:nth-child(3) a.b').innerText;
-        const type = tr_node.querySelector('td:nth-child(3) span.cat').innerText;
-        return [type, date, appid, title];
-    }).filter(arr => arr[0] === 'Demo').map(arr => arr.slice(1).join('^')).join('\n');
+    let output_text_arr = [];
+    document.querySelectorAll('tr.app').forEach(function (tr_node) {
+        try {
+            const appid = tr_node.getAttribute('data-appid');
+            console.log(`appid = ${appid}`);
+            console.log(`tr.app[data-appid="${appid}"] td:nth-child(7)`);
+            const date = document.querySelector(`tr.app[data-appid="${appid}"] td:nth-child(7)`).getAttribute(`data-sort`);
+            const title = document.querySelector(`tr.app[data-appid="${appid}"] td:nth-child(3) a.b`).innerText;
+            const type = document.querySelector(`tr.app[data-appid="${appid}"] td:nth-child(3) span.cat`).innerText;
+            if (type === 'Demo') {
+                output_text_arr.push([type, date, appid, title])
+            };
+        }catch(e){
+        }
+    })
+    const output_text = output_text_arr.map(arr => arr.slice(1).join('^')).join('\n');
+    console.log(output_text_arr);
     console.log(output_text);
     copy(output_text);
     // Get clipboard content: xclip -selection clipboard -o
